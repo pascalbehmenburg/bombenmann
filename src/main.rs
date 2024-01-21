3544a27cc0d57203742ab20fcd02c44d98fb2936
pascalbehmenburg <pascalbehmenburg@posteo.net>
2024-01-21 14:50:26 +0100

    add basic movement using rapier2d physics engine

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,90 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+use bevy::window::{WindowMode, PresentMode};
+use bevy::input::InputSystem;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                present_mode: PresentMode::Fifo,
+                mode: WindowMode::Windowed,
+                fit_canvas_to_parent: true,
+                ..default()
+            }),
+            ..default()
+        }).set(ImagePlugin::default_nearest())
+        .build(), RapierPhysicsPlugin::<NoUserData>::default(), RapierDebugRenderPlugin{enabled: true, ..default()}, InputPlugin))
         .add_systems(Startup, startup)
-        .add_systems(Update, (sprite_movement, camera_movement))
+        .add_systems(Update, movement)
         .run();
-}
-
-#[derive(Component)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
 }
 
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
+            RigidBody::Dynamic,
+            Velocity::zero(),
         SpriteBundle {
             texture: asset_server.load("player.png"),
             transform: Transform::from_xyz(100., 0., 0.),
             ..default()
-        },
-        Direction::Down,
+        }
     ));
 }
 
-fn sprite_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut sprite_position: Query<(&mut Direction, &mut Transform)>,
-) {
-    static SPEED: f32 = 4.;
-    for (mut direction, mut transform) in &mut sprite_position {
-        if keyboard_input.pressed(KeyCode::W) {
-            *direction = Direction::Up;
-            transform.translation.y += SPEED;
-        }
-        if keyboard_input.pressed(KeyCode::A) {
-            *direction = Direction::Left;
-            transform.translation.x -= SPEED;
-        }
-        if keyboard_input.pressed(KeyCode::S) {
-            *direction = Direction::Down;
-            transform.translation.y -= SPEED;
-        }
-        if keyboard_input.pressed(KeyCode::D) {
-            *direction = Direction::Right;
-            transform.translation.x += SPEED;
-        }
+#[derive(Resource, Default)]
+struct PlayerInput {
+    pub move_direction: Vec2
+}
+
+pub struct InputPlugin;
+
+impl Plugin for InputPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            PreUpdate,
+            (
+               movement_input 
+            )
+                .after(InputSystem),
+        )
+        .init_resource::<PlayerInput>();
     }
+}
+
+fn movement_input(
+    keys: Res<Input<KeyCode>>,
+    mut player_input: ResMut<PlayerInput>
+) {
+    let mut direction = Vec2::default();
+
+    if keys.pressed(KeyCode::S) {
+        direction += Vec2::new(0.0, -1.0);
+    }
+    if keys.pressed(KeyCode::W) {
+        direction += Vec2::new(0.0, 1.0);
+    }
+    if keys.pressed(KeyCode::D) {
+        direction += Vec2::new(1.0, 0.0);
+    }
+    if keys.pressed(KeyCode::A) {
+        direction += Vec2::new(-1.0, 0.0);
+    }
+
+    player_input.move_direction = direction.normalize_or_zero();
+}
+
+fn movement(
+    mut q_velocity: Query<&mut Velocity>,
+    player_input: Res<PlayerInput>
+    ) {
+        for mut velocity in &mut q_velocity { 
+            if player_input.move_direction == Vec2::default() {
+                velocity.linvel = Vec2::ZERO;
+                return;
+            }
+
+            velocity.linvel = player_input.move_direction * 200.0;
+        }
 }
